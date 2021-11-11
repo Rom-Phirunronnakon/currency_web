d0463a574958f40983d5d5dce06e41c3055ee16e
Rom-Phirunronnakon <64011598@kmitl.ac.th>
2021-11-11 10:58:18 +0000

    My first commit

---
+++ flask_app.py
@@ -0,0 +1,23 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Rom Phirunronnakon'
+
+@app.route('/aaaa')
+def hello_world_aaaa():
+    return 'the path is aaaa'
+
+@app.route('/aaaa/bbbb')
+def hello_world_aaaa_bbbb():
+    return 'the path is aaaa bbbb'
+
+@app.route('/today')
+def today_date():
+    return str(date.today())
