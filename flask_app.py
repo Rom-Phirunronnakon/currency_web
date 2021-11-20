01f192f98250e78c09ff1cb3cd7ff6def42cbce9
Rom Phirunronnakon <64011598@kmitl.ac.th>
2021-11-20 11:15:31 +0700

    change back to py

---
+++ flask_app.py
@@ -0,0 +1,18 @@
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Rom Phirunronnakon'
+
+@app.route('/today')
+def today_date():
+    return str(date.today())
+
+@app.route('/handsome')
+def handsome():
+    return 'You are so handsome!'
