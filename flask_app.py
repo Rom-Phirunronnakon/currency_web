a3e22542a4f7c6d3d59223122dec866ab15e4633
Rom Phirunronnakon <64011598@kmitl.ac.th>
2021-11-22 10:58:52 +0700

    test

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,9 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template, request
 from datetime import date
+import requests
 
 app = Flask(__name__)
 
@@ -14,4 +15,16 @@ def hello_world():
 def hello_today():
     return str(date.today())
 
+@app.route('/stock')
+def stock_form():
+    return render_template('form.html')
 
+@app.route('/stockresult', methods = ['GET'])
+def stock_result():
+    stock_symbol = request.args.get('amount')
+    result = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%22+stock_symbol+%22&apikey=Q3TONPUKGTPM3WQQ%22")
+    jsondata = result.json()
+
+    stock_data = jsondata["Time Series (Daily)"]
+
+    return render_template('stock_show.html',stock_symbol = stock_symbol, stock_data= stock_data)
